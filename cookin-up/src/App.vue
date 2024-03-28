06996266ff6c99450d6d591ff135daa6a4094f21
Gustavo Henrique Ferreira <gustavohenriqueferreira110@gmail.com>
2024-03-28 11:00:05 -0300

    Update

--- cookin-up/src/App.vue
+++ cookin-up/src/App.vue
@@ -1,3 +1,19 @@
+<script lang="ts">
+import Banner from './components/Banner.vue'
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: { 
+    Banner:Banner,
+    ConteudoPrincipal:ConteudoPrincipal ,
+    Footer:Footer
+  }
+}
+</script>
+
 <template>
-  <h1>Projeto Vue</h1>
+  <Banner />
+  <ConteudoPrincipal />
+  <Footer />
 </template>
---
+++ cookin-up/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="footer">
+        <h1 class="title">Footer</h1>
+    </section>
+</template>
+
+<style scoped>
+.footer {
+    background-color: rgb(35, 75, 19);
+    
+    display: flex;
+    padding: 20px;
+    flex-direction: column;
+    align-items: center;
+    gap: 5rem;
+}
+.title {
+    font-size: 16px;
+}
+</style>
